48390a8f4250c8a6f45df99b883040efedfbff43
future，i'm yan <48935623+yanabcd@users.noreply.github.com>
2020-12-15 10:03:55 +0800

    项目一

---
+++ 项目一/项目一/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 项目一
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+            label3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sno, sname;
+            sno = textBox1.Text;
+            sname = textBox2.Text;
+            label3.Text = "学号" + sno + "姓名" + sname;
+        }
+    }
+}
