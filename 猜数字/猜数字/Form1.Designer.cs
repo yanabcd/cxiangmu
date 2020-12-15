705ca85842e0f8a761aeed5e247cfad13a5ab34c
future，i'm yan <48935623+yanabcd@users.noreply.github.com>
2020-12-15 15:12:48 +0800

    猜数字2

---
+++ 猜数字/猜数字/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace 猜数字
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.使用说明toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.使用说明toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "题目区";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("华文中宋", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(247, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 80);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "欢迎来到数字天地";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(639, 253);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 36);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "产生随机数";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(652, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "提交答案";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(123, 447);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "退出";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(652, 447);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 36);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "重新开始";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // 使用说明toolStrip1
+            // 
+            this.使用说明toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.使用说明toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.使用说明toolStrip1.Name = "使用说明toolStrip1";
+            this.使用说明toolStrip1.Size = new System.Drawing.Size(1052, 25);
+            this.使用说明toolStrip1.TabIndex = 10;
+            this.使用说明toolStrip1.Text = "使用说明";
+            this.使用说明toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.使用说明toolStrip1_ItemClicked);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox1.Text = "使用说明";
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 243);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 59);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(637, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "答题区";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 539);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.使用说明toolStrip1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.使用说明toolStrip1.ResumeLayout(false);
+            this.使用说明toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolStrip 使用说明toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 猜数字/猜数字/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 猜数字
+{
+    public partial class Form1 : Form
+    {
+        int number = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 使用说明toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+        //题目区
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            number = rd.Next(1, 100);
+            textBox1.Text = string.Format("已经产生了一个（1-100）（不包括100）的整数，请在答题区输入你的答案！");
+            button2.Enabled = false;
+
+        }
+        //答题区
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = textBox2.Text;
+            int number1;
+            bool x = int.TryParse(a, out number1);
+            if (x)
+            {
+                number1=int.Parse(a);
+                if (number1 >= 1 && number1 < 100)
+                {
+                    if(number1>number)
+                    {
+                        MessageBox.Show("高了");
+                        textBox2.Clear();
+                    }
+                    else if(number1<number){
+                        MessageBox.Show("低了");
+                        textBox2.Clear();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("恭喜你答对了");
+                        textBox2.Clear();
+                        button3.Enabled = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("2请输入1-100(不包括100)的整数");
+                    textBox2.Clear();
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("1请输入1-100(不包括100)的整数");
+                textBox2.Clear();
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.点击按钮“产生随机数”随机产生一个1-100的数" + "\r\n" + "2.在答题区输入答案并提交");
+        }
+      
+        
+        
+    }
+}
